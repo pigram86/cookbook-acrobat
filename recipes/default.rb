fe2d17dd6b40683630cb43ad5f75d2c75758ff83
pigram86 <todd@toddpigram.com>
2013-12-02 12:37:24 -0500

    attr added

---
+++ recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: acrobat
+# Recipe:: default
+#
+# Copyright (C) 2013 Todd Pigram
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Install acrobat Reader 11.0
+windows_package "AdbeRdr11000_en_US" do
+  source node[:acrobat][:install_url]
+  action :install
+  not_if {::File.exists?(nodet[:acrobat][:acrord])}
+  not_if {reboot_pending?}
+end
+
+# disable acrobat update
+registry_key node[:acrobat][:featurelockdown] do
+  values [{
+    :name => "bUpdater",
+    :type => :dword,
+    :data => 00000000
+    }]
+  recursive true
+  action :create
+end
+
+# if feature installs, schedule a reboot at end of chef run
+windows_reboot 60 do
+  reason 'reboot needed'
+  only_if {reboot_pending?}
+end 
